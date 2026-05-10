9cd0fc98e91d0016fb9a777705bcc8ddb767d567
Sarita Mandal <mandalsarita11@gmail.com>
2026-05-10 10:54:21 +0530

    added validations to user schema

--- src/app.js
+++ src/app.js
@@ -5,21 +5,69 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/user', async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.findOne({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send('user not found');
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send('Something went wrong!');
+  }
+});
+
+app.delete('/user', async (req, res) => {
+  const userId = req.body._id;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send('user deleted successfully');
+  } catch (err) {
+    res.status(400).send('Something went wrong!');
+  }
+});
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send('Something went wrong!');
+  }
+});
+
 app.post('/signUp', async (req, res) => {
   const user = new User(req.body);
-  try{
-   await user.save();
-   res.send('user added successfully')
-  } catch(err){
-    res.status(400).send('Error saving user data:'+err.message);
+  try {
+    await user.save();
+    res.send('user added successfully');
+  } catch (err) {
+    res.status(400).send('Error saving user data:' + err.message);
   }
+});
 
-})
-connectDB().then(() => {
-  console.log('database connected successfully.');
-  app.listen(7777, () => {
-  console.log('server is listing on port 7777...');
+app.patch('/user', async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: 'after',
+      runValidators: true
+    });
+    res.send('user updated successfully');
+  } catch (err) {
+    res.status(400).send('Something went wrong!'+err.message);
+  }
 });
-}).catch((err) =>{
-  console.log('cannot connect to database.', err);
-});
+
+connectDB()
+  .then(() => {
+    console.log('database connected successfully.');
+    app.listen(7777, () => {
+      console.log('server is listing on port 7777...');
+    });
+  })
+  .catch((err) => {
+    console.log('cannot connect to database.', err);
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,49 @@ const mongoose = require('mongoose');
 const userSchema =new mongoose.Schema({
   firstName: {
     type: 'String',
+    required: true,
+    minLength:4,
+    maxLength:50
   },
   lastName: {
     type: 'String',
   },
   emailId: {
     type: 'String',
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
   },
   password: {
     type: 'String',
+    required: true,
   },
   age: {
     type: 'Number',
+    min: 18
   },
   gender: {
     type: 'String',
+    validate(value){
+      if(!['male', 'female','others'].includes(value)){
+        throw new Error('not a valid gender!!!')
+      }
+    }
   },
+  photoUrl: {
+    type:'String',
+    default: 'https://as2.ftcdn.net/jpg/03/31/69/91/1000_F_331699188_lRpvqxO5QRtwOM05gR50ImaaJgBx68vi.jpg'
+  },
+  about: {
+    type: "String",
+    default: "this is a default about of user"
+  },
+  skills: {
+    type: ["String"]
+  }
+},{
+  timestamps: true
 });
 
 const User = mongoose.model('user', userSchema);
